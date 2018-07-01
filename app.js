36852ddebf3e3e4602af806f94fae3e8df8c145b
pr-olga <frkhorkovaolga@gmail.com>
2018-07-01 23:44:35 +0200

    feat: adding service worker
    Regsiter SW, then install, activate and fetch caches

---
+++ app.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('sw.js', {scope: './'})
+        .then(function (registration) {
+            console.log("Service Worker Registred", registration);
+        })
+.catch(function(err){
+    console.log("Registration failes", err);
+})
+
+}
