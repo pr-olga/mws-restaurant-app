191e4cd215e10eaf512183c5dd141525e7c52c35
pr-olga <frkhorkovaolga@gmail.com>
2018-07-07 14:07:51 +0200

    fix: adjust SW scope to the GitHub Pages location
    change / to /mws-restaurant-stage-1/

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('sw.js', {scope: '/'})
+        .register('sw.js', {scope: '/mws-restaurant-stage-1/'})
         .then(function (registration) {
             console.log("Service Worker Registred", registration);
         })
