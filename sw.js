dfad5ad52108e68d2158f5239876c61507592b5a
pr-olga <frkhorkovaolga@gmail.com>
2018-07-07 13:54:27 +0200

    fix: change the path to the cached files
    chenge / to ./

--- sw.js
+++ sw.js
@@ -1,25 +1,24 @@
 var staticCacheName = 'version1';
 var cacheFiles = [
-    '/',
-    '/index.html',
-    '/restaurant.html',
-    '/css/styles.css',
-    '/img/1.jpg',
-    '/img/2.jpg',
-    '/img/3.jpg',
-    '/img/4.jpg',
-    '/img/5.jpg',
-    '/img/6.jpg',
-    '/img/7.jpg',
-    '/img/8.jpg',
-    '/img/9.jpg',
-    '/img/10.jpg',
-    '/js/main.js',
-    '/js/dbhelper.js',
-    '/js/restaurant_info.js',
-    '/js/picturefill.js',
-    '/data/restaurants.json',
-    '/app.js',
+    './',
+    './restaurant.html',
+    './css/styles.css',
+    './img/1.jpg',
+    './img/2.jpg',
+    './img/3.jpg',
+    './img/4.jpg',
+    './img/5.jpg',
+    './img/6.jpg',
+    './img/7.jpg',
+    './img/8.jpg',
+    './img/9.jpg',
+    './img/10.jpg',
+    './js/main.js',
+    './js/dbhelper.js',
+    './js/restaurant_info.js',
+    './js/picturefill.js',
+    './data/restaurants.json',
+    './app.js',
     'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1206/1539.jpg70?access_token=pk.eyJ1IjoicHItb2xnYSIsImEiOiJjaml3MmEzcDcwZjF6M2tvc2dsc2xhcW9jIn0.C3eNrk94kkZnqDbrgN4pow ',
     'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1206/1540.jpg70?access_token=pk.eyJ1IjoicHItb2xnYSIsImEiOiJjaml3MmEzcDcwZjF6M2tvc2dsc2xhcW9jIn0.C3eNrk94kkZnqDbrgN4pow',
     'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1207/1540.jpg70?access_token=pk.eyJ1IjoicHItb2xnYSIsImEiOiJjaml3MmEzcDcwZjF6M2tvc2dsc2xhcW9jIn0.C3eNrk94kkZnqDbrgN4pow',
@@ -31,6 +30,7 @@ var cacheFiles = [
     'https://unpkg.com/leaflet@1.3.1/dist/images/marker-shadow.png',
     'https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon-2x.png',
     'https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon.png'
+
 ]
 
 self.addEventListener('install', function (e) {
@@ -58,12 +58,12 @@ self.addEventListener('activate', function (e) {
     return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
     console.log(`Fetching FROM: ${event.request.url}`)
     event.respondWith(
         // open the certain cache
-        caches.open('version1').then(function(cache) {
-            return cache.match(event.request).then(function(response) {
+        caches.open('version1').then(function (cache) {
+            return cache.match(event.request).then(function (response) {
                 return response || fetch(event.request);
             });
         })
